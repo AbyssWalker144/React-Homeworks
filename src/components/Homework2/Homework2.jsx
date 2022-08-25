6d6c449638c82716a83d8552438d007cd170a0cf
Valeriy <balorok5@gmail.com>
2022-08-25 21:34:09 +0300

    Homework3

--- src/components/Homework2/Homework2.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Homework2 extends Component {
-  render() {
-    return (
-      <div>Homework2 - {this.props.usefulMessage}</div>
-    )
-  }
-}
